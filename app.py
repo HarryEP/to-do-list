8ff38adacdb5b613530ceda09174010b1e8ad288
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-05-19 17:14:37 +0100

    index route implemented using the database|

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-def get_connection(host: str, db_name: str, password: str, user: str) -> connection|None:
+
+def get_connection(host: str, db_name: str, password: str, user: str) -> connection | None:
     '''Connects to the database'''
     try:
         conn = psycopg2.connect(host=host,
@@ -18,6 +21,18 @@ def get_connection(host: str, db_name: str, password: str, user: str) -> connect
         print(f"Error {e} occured!")
         return None
 
-if __name__ == "__main__":
 
+@app.route('/')
+def index():
+    conn = get_connection(os.environ["DB_HOST"], os.environ["DB_NAME"],
+                          os.environ["DB_PASS"], os.environ["DB_USER"])
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM todolist;")
+        todolist = cur.fetchall()
+    conn.close()
+    return jsonify(todolist)
+
+
+if __name__ == "__main__":
+    load_dotenv()
     app.run(debug=True)
