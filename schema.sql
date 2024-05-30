5d00ee8e54a0d31444697661ccd9711f7fedf132
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-05-30 20:12:32 +0100

    modified sql

--- schema.sql
+++ schema.sql
@@ -7,6 +7,11 @@ CREATE TABLE IF NOT EXISTS todolist (
     todo_id INT GENERATED ALWAYS AS IDENTITY,
     item TEXT NOT NULL,
     priority INT NOT NULL DEFAULT 1,
-    done BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (todo_id)
+);
+
+CREATE TABLE IF NOT EXISTS completed (
+    completed_id INT GENERATED ALWAYS AS IDENTITY,
+    item TEXT NOT NULL,
+    PRIMARY KEY (completed_id)
 );
