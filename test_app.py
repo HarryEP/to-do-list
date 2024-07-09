2c1ca7eb6f8f5b598ea8abed648d02cea7632a1b
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-07-09 08:48:45 +0100

    added tests about the errorhandling

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch, MagicMock
+import psycopg2
 
 
 def test_get_home_route_returns_successful_status_code(test_api):
@@ -9,7 +10,7 @@ def test_get_home_route_returns_successful_status_code(test_api):
 
 
 @patch('app.get_connection')
-def test_post_function_returns_successful_status_code(mock_get_connection, test_api):
+def test_post_item_successfully_adds_item(mock_get_connection, test_api):
     mock_conn = MagicMock()
     mock_get_connection.return_value = mock_conn
 
@@ -19,3 +20,17 @@ def test_post_function_returns_successful_status_code(mock_get_connection, test_
     assert res.status_code == 302
     assert res.location.endswith('/')
     mock_conn.commit.assert_called_once()
+
+
+def test_404(test_api):
+    response = test_api.get('/error')
+    assert response.status_code == 404
+    assert b'Page Not Found' in response.data
+
+
+@patch('app.get_connection')
+def test_500(mock_get_connection, test_api):
+    mock_get_connection.side_effect = psycopg2.Error
+    response = test_api.get('/')
+    assert response.status_code == 500
+    assert b'Internal Server Error' in response.data
